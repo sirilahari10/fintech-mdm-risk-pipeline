78b00996b20c6cf5c6ef8b1224264cb3ef11c16a
sirilahari10 <sxc6128@mavs.uta.edu>
2026-09-01 23:12:38 -0500

    Create fintech_pipeline_dag.py

---
+++ dags/fintech_pipeline_dag.py
@@ -0,0 +1,29 @@
+"""
+Orchestrates the PySpark MDM job, followed by Snowflake data quality checks, 
+and dbt dimensional modeling.
+"""
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG('fintech_risk_pipeline', schedule_interval='@daily', start_date=datetime(2026, 9, 1)) as dag:
+
+    # 1. Run PySpark Identity Resolution (MDM)
+    run_mdm_spark_job = SparkSubmitOperator(
+        task_id='run_pyspark_mdm',
+        application='/jobs/identity_resolution.py',
+        name='member_identity_resolution'
+    )
+
+    # 2. Run dbt to build the dimensional models in Snowflake
+    run_dbt_models = BashOperator(
+        task_id='dbt_run_risk_models',
+        bash_command='dbt run --models tag:risk_analytics --profiles-dir /dbt'
+    )
+
+    # 3. Test for data quality and PII compliance
+    test_dbt_models = BashOperator(
+        task_id='dbt_test',
+        bash_command='dbt test'
+    )
